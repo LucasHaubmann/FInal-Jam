2fe0821f3f36af2050ea39988639319db58087d4
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2025-06-23 18:24:01 -0300

    Server Atualizado

--- server/index.js
+++ server/index.js
@@ -138,7 +138,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3001, '0.0.0.0', () => {
+  console.log("Servidor rodando na porta 3001");
 });
